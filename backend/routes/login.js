84010906673a078f8ac8b4ca7650c9fba7b610a6
Harshil Dumasia <harshildumasia@gmail.com>
2025-07-03 16:29:50 +0530

    updated to send food list

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -7,7 +7,7 @@ router.post('/',async(req,res)=>{
     try {
         let user = await User.findOne({ email,password});
         if (user) {
-            return res.json({ msg: 'User logged In' });
+            return res.json({ msg: 'User logged In',data:user });
         }
         else{
             res.json("user not found");
